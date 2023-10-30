0bb5294bbd33a1c2bd28667db79d057ee11cd152
ment228 <tosmenttrevor@gmail.com>
2023-10-30 20:01:51 +0300

    выполнено\

--- лб9/задание1в4.py
+++ лб9/задание1в4.py
@@ -1,18 +1,13 @@
 ''' Даны две дроби A/B и C/D (А, В, С, D — натуральные числа).
 Составить программу деления дроби на дробь. Ответ должен быть несократимой дробью.
 Использовать подпрограмму алгоритма Евклида для определения НОД. '''
-def gcd(a, b):
-    while b != 0:
-    a, b = b, a % b
-    return a
-def divide_fraction(a, b, c, d):
-    numerator = a * d
-    denominator = b * c
-    divisor = gcd(numerator, denominator)
-    return numerator, denominator
-A = 3
-B = 5
-C = 2
-D = 7
-result = divide_fraction(A, B, C, D)
-print("Результат деления:", result[0], "/", result[1])
+from math import gcd
+
+a = int(input('введите число a: '))
+b = int(input('введите число b: '))
+c = int(input('введите число c: '))
+d = int(input('введите число d: '))#Запрашиваем пользователя ввести числа
+F1 = a*d#Умножение чисел в переменной F1
+F2 = b*c#Умножение чисел в переменной F2
+t = gcd(F1, F2)#Считаем НОД
+print(F1 // t ,'/', F2 // t)#Выводим результат
